17f0560682c60addad3b1d2db7facc7b59161575
Anatoli Nicolae <desk@anatolinicolae.com>
2021-01-24 14:40:31 +0100

    Fix hooks
    Signed-off-by: Anatoli Nicolae <desk@anatolinicolae.com>

--- src/sqrd/Composer/Hooks.php
+++ src/sqrd/Composer/Hooks.php
@@ -6,12 +6,20 @@ use Composer\Script\Event;
 
 class Hooks
 {
-    const APISDIR = '/usr/local/apnscp/config/custom';
+    const APISDIR = '/usr/local/apnscp';
+    const APISCSTM = '/usr/local/apnscp/config/custom';
     public static function postUpdate(Event $event) : void
     {
-        mkdir(static::APISDIR . '/apps/login/views/partials/', 755, true);
-        copy('src/sqrd/ApisCP/Extensions/views/status.blade.php', static::APISDIR . '/apps/login/views/partials/status.blade.php');
-        copy('src/sqrd/ApisCP/Extensions/BetterUptime.php', static::APISDIR . '/BetterUptime.php');
-        echo "You're done!";
+        if (!is_dir(static::APISCSTM . '/apps/login/views/partials/'))
+        {
+            mkdir(static::APISCSTM . '/apps/login/views/partials/', 0755, true);
+        }
+
+        copy('src/sqrd/ApisCP/Extensions/views/status.blade.php', static::APISCSTM . '/apps/login/views/partials/status.blade.php');
+        copy('src/sqrd/ApisCP/Extensions/BetterUptime.php', static::APISCSTM . '/BetterUptime.php');
+
+        exec(sprintf('cd %s ; composer dumpautoload -o', static::APISDIR));
+
+        echo "You're done!" . PHP_EOL;
     }
 }
